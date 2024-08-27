1cef54100df18b0661de0cfdf18209be8d62022c
Kyle Greenidge <kylegreen149@gmail.com>
2024-08-27 16:25:02 -0400

    Done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    if length == 0:
+        fibonacci = []
+    if length == 1:
+        fibonacci = [0]
+    if length > 1:
+        fibonacci = [0, 1]
+
+    while len(fibonacci) < length :
+        total = fibonacci[-2] + fibonacci[-1]
+        fibonacci.append(total)
+    print(fibonacci)
